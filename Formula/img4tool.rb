d299a32fad9ddab3cf501df18e1f7d243b200a97
Alexis Deruelle <alexis.deruelle@gmail.com>
2022-11-13 12:16:00 +0100

    fix audit error for img4tool and libgeneral formulaes
    Signed-off-by: Alexis Deruelle <alexis.deruelle@gmail.com>

--- Formula/img4tool.rb
+++ Formula/img4tool.rb
@@ -1,39 +1,25 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Img4tool < Formula
-  desc "A tool for manipulating IMG4, IM4M and IM4P files"
+  desc "Tool for manipulating IMG4, IM4M and IM4P files"
   homepage "https://github.com/tihmstar/img4tool"
   url "https://github.com/tihmstar/img4tool.git",
       using:    :git,
-      tag:      "197"
-  license "LGPL-3.0"
+      tag:      "197",
+      revision: "aca6cf005c94caf135023263cbb5c61a0081804f"
+  license "LGPL-3.0-only"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "libgeneral" => :build
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  depends_on 'pkg-config' => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "./autogen.sh"
     system "./configure", *std_configure_args, "--disable-silent-rules"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test img4tool`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
--- Formula/libgeneral.rb
+++ Formula/libgeneral.rb
@@ -1,38 +1,25 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Libgeneral < Formula
-  desc "libgeneral is a collection of c++ macros, that help follow a consistent coding style"
+  desc "Collection of c++ macros, that help follow a consistent coding style"
   homepage "https://github.com/tihmstar/libgeneral"
   url "https://github.com/tihmstar/libgeneral.git",
       using:    :git,
-      tag:      "63"
-  license "LGPL-2.1"
+      tag:      "63",
+      revision: "017d71edb0a12ff4fa01a39d12cd297d8b3d8d34"
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  depends_on 'pkg-config' => :build
+  license "LGPL-2.1-only"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "./autogen.sh"
     system "./configure", *std_configure_args, "--disable-silent-rules"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test libgeneral`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
